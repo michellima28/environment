799a17ae4175e88b5fe947ad7b8f44a566a3f0f9
root <root@localhost.localdomain>
2020-07-13 16:04:42 -0300

    Added python 3.5.9 and jupyterlab

--- fedora/default_apps.sh
+++ fedora/default_apps.sh
@@ -9,3 +9,20 @@
 echo "installing vim-enhanced..."
 sudo dnf install vim-enhanced
 
+# python 3.5.9
+echo "installing python 3.5.9..."
+sudo yum install gcc sqlite-devel
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.5.9/Python-3.5.9.tgz
+sudo tar xzf Python-3.5.9.tgz
+cd Python-3.5.9
+./configure --enable-optimizations
+sudo make altinstall
+cd ..
+sudo rm Python-3.5.9.tgz
+
+# jupyterlab
+echo "installing jupyterlab..."
+pip3 install jupyterlab --user
+jupyter serverextension enable --py jupyterlab
+
