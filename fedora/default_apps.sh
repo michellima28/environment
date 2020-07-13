44d58c302c7b261d9729327ced96825afdb1e48a
root <root@localhost.localdomain>
2020-07-12 23:15:39 -0300

    Included echo message

--- fedora/default_apps.sh
+++ fedora/default_apps.sh
@@ -4,7 +4,8 @@
 # it was tested on Fedora 31
 
 # vim editor
-sudo dnf update
-echo "updating dnf packages..."
-sudo dnf install vim-enhanced
+#echo "updating dnf packages..."
+#sudo dnf update
 echo "installing vim-enhanced..."
+sudo dnf install vim-enhanced
+
